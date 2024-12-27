0fddf4ba35d4d5b333fbcd903eec73a8bd8fbed1
Alex Luo <60013046+AlexLuo602@users.noreply.github.com>
2024-12-26 22:06:55 -0800

    Set up test file for pdf processing

---
+++ file_parser_service/tests/test_file_parser_engine.py
@@ -0,0 +1,16 @@
+import unittest
+from file_parser_service.file_parser_engine import parse_pdf
+
+# to test:
+# in directory, run python -m tests.test_file_parser_engine.py
+
+class TestLocalize(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_template(self, stub_param):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
